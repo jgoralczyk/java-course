de265368523eb46dc07cb74219925456594219ad
jgoralczyk <kubaty03@gmail.com>
2022-06-19 20:51:08 +0200

    update

--- boxingi/src/pl/gural/Bank.java
+++ boxingi/src/pl/gural/Bank.java
@@ -34,6 +34,7 @@ public class Bank {
         }
         return false;
     }
+
     private Branch findBranch(String branchName){
         for(int i=0; i<this.branches.size(); i++){
             Branch checkedBranch= this.branches.get(i);
@@ -44,63 +45,55 @@ public class Bank {
         return null;
     }
 
-    public boolean listCustomers(String branchName, boolean showTransactions{
+    public boolean listCustomers(String branchName, boolean showTransactions {
         Branch branch = findBranch(branchName);
-        if(branch !=null){
+        if (branch != null) {
             System.out.println("Customers details for branch " + branch.getName());
 
-            ArrayList<Customer> branchCustomers=branch.getCustomers();
-            for(int i=0; i<branchCustomers.size(); i++){
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for (int i = 0; i < branchCustomers.size(); i++) {
                 Customer branchCustomer = branchCustomers.get(i);
-                System.out.println("Customer: " + branchCustomer.getName()+ "["+ (i+1) +"]");
-                if(showTransactions){
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i + 1) + "]");
+                if (showTransactions) {
                     System.out.println("Transactions");
-                    ArrayList<Double> transactions= branchCustomer.getTransactions();
-                    for(int j=0; j< transactions.size(); j++){
-                        System.out.println("["+(j+1) + "] Amount " + transactions.get(j));
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j + 1) + "] Amount " + transactions.get(j));
                     }
                 }
             }
             return true;
-        }else{
+        } else {
             return false;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
